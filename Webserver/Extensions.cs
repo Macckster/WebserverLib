f383fd9280fec4515e7be5047993a75ad3c00f09
Macckster <35381093+Macckster@users.noreply.github.com>
2019-01-09 19:26:02 +0100

    Initial Code Commit

---
+++ Webserver/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Webserver.HTML
+{
+    static class Extensions
+    {
+        static string GetURLPart(this HttpListenerRequest req, int index)
+        {
+            string part = req.RawUrl.Split('/')[index];
+
+            return part;
+        }
+    }
+}
---
+++ Webserver/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Webserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebServer ws = new WebServer(SendResponse, "http://localhost:8080/test/");
+            ws.Run();
+            Console.WriteLine("Press a key to quit.");
+            Console.ReadKey();
+            ws.Stop();
+        }
+
+        public static string LoadHTML(string path)
+        {
+            return File.ReadAllText(path);
+        }
+
+        public static string SendResponse(HttpListenerRequest request)
+        {
+            string a = LoadHTML("HTML/Test.html");
+
+            string conv = "";
+
+            if (request.HasParameter("URL"))
+            {
+                conv = ConvertToBase64(request.GetParameter("URL"));
+            }
+
+            a = string.Format(a, conv);
+
+            return a;
+        }
+
+        public static string ConvertToBase64(string text)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(text);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+    }
+}
